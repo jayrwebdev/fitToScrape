e72bb06889615f169d9ed63b37cb29b159f8f316
Jahaan Rasheed <jahaanrasheed@Jahaans-MacBook-Pro.local>
2019-10-15 22:36:31 -0400

    added body-parser and handlebars code

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require("express")
 const expbs = require("express-handlebars");
 const PORT = process.env.PORT || 3200
+const bodyParser = require("body-parser")
 const scrape = express()
+// for body Parser
+scrape.use(bodyParser.urlencoded({ extended: true }));
+scrape.use(bodyParser.json());
 
+//Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Handlebars
+scrape.engine(
+    "handlebars",
+    exphbs({
+      defaultLayout: "main"
+    })
+  );
+  scrape.set("view engine", "handlebars");
 
 scrape.listen(PORT, err =>{
    if (err) {
@@ -11,3 +28,5 @@ scrape.listen(PORT, err =>{
        console.log("The server connection was succesful")
    }
 })
+
+module.exports = scrape
